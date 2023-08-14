d6aa4eb4fc0126222e34e45e561e788bbe718496
Sara Garcia <saragca81@gmail.com>
2023-08-13 17:59:04 -0700

    added code to js

--- script/main.js
+++ script/main.js
@@ -2,4 +2,17 @@ var ApiKey = "7e2c9dd2ce3aec612d1cb1d25cf47214";
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+function getApi(){
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+}
+
 fetch(queryURL)
+.then(function (response){
+    return response.json();
+})
+// .then(function (data) {
+//     for(var i = 0; < data.length; i++){
+//        var listItem = document. 
+//     }
+// })
