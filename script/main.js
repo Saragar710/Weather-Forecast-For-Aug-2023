92418f1a04df6c4377b26160ed55f3051517196f
Sara Garcia <saragca81@gmail.com>
2023-08-24 14:07:22 -0700

    updated more js

--- script/main.js
+++ script/main.js
@@ -2,8 +2,32 @@ var APIKey = "7e2c9dd2ce3aec612d1cb1d25cf47214";
 
 var cityInput = document.getElementById("city-name");
 var formButton = document.querySelector("form button");
-var createWeatherCard = (weatherItem) => {
-    return ``;
+var currentWeatherDiv = document.querySelector(".current-weather");
+var weatherCardsDiv = document.querySelector(".weather-cards");
+
+var createWeatherCard = (CityName, weatherItem, index) => {
+    if(index === 0) {
+        return `   <div class="details">
+        <h3>${cityName}(${weatherItem.dt_txt.split(" ")[0]})</h3>
+        <h4>Temperature:${weatherItem.main.temp}</h4>
+        <h4>Wind Speed: ${weatherItem.wind.speed}mph</h4>
+        <h4>Humidity:${weatherItem.main.humidity} %</h4>
+      </div>
+      <div class="icon">
+      <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon} /@2x.png" alt="weather-icon">
+      <h4>${weatherItem.weather[0].description}</h4>
+    </div>`;
+
+
+    } else {
+    return `<li class="weather-cards">
+           <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+           <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}2x.png" alt="weather-icon">"
+           <h4>Temperature: ${weatherItem.main.temp}</h4>
+           <h4>Wind Speed: ${weatherItem.wind.speed}mph</h4>
+           <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+    </li>`;
+    }
 }
 
 
@@ -69,14 +93,29 @@ function getApi(city){
         if(!uniqueForecastDate.includes(forecastDate)) {
             return uniqueForecastDate.push(forecast);
         }
-    });
-    console.log(fiveDayForecast);
-    fiveDayForecast.fo;rEach(weatherItem => {
-        createWeatherCard(weatherItem);
-    });
+    })
 
- }   .catch(() => {
-    alert("Error")
+
+    cityInput.value = "";
+    currentWeatherDiv.innerHTML = "";
+    weatherCardsDiv.innerHTML = "";
+
+    console.log(fiveDayForecast);
+    fiveDayForecast.forEach((weatherItem, index) => {
+        if(index === 0) {
+        weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+
+        } else{
+
+        weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+        }// createWeatherCard(weatherItem);
+    })
+
+ }; 
+
+  .catch(() => {
+    alert("Error");
 });
+searchButton.addEventListener("click", getSelectedCity);
 
 
