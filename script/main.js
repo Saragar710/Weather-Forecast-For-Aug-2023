361628f441412ccc8c2bd9552cbdbd893a0c48bf
Sara Garcia <saragca81@gmail.com>
2023-08-31 22:38:46 -0700

    updated javascript

--- script/main.js
+++ script/main.js
@@ -6,6 +6,7 @@ var formButton = document.querySelector("form button");
 var currentWeatherDiv = document.querySelector(".current-weather");
 var weatherCardsDiv = document.querySelector(".weather-cards");
 var cityDataArray = [];
+var historyBtn = document.getElementById("history-btn")
 //passes city input to get geo coordiantes (name, lat, lon)
 function getApi(city) {
     var queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIKey}
@@ -22,30 +23,30 @@ function getApi(city) {
         .then(function (data) {
             console.log(data[0]);
             //get weather
-          getWeather(data[0])
-       //take city input to another function to send to local storage
-           cityHistory(city);
+            getWeather(data[0])
+            //take city input to another function to send to local storage
+            cityHistory(city);
         })
-}  
+}
 //send input of city to local storage
 function cityHistory(city) {
     console.log(city)
     cityDataArray.push(city)
-    
+
     //send to local storage
     localStorage.setItem("cityData", JSON.stringify(cityDataArray))
-  //iterating / 4loop the array to create buttons to display
-  displayCityArray()
+    //iterating / 4loop the array to create buttons to display
+    displayCityArray()
 
 }
 //getting array from location storage
 function getCityArray() {
     var storedCity = localStorage.getItem('cityData')
-    if(storedCity){
+    if (storedCity) {
         cityDataArray = JSON.parse(storedCity)
     }
     //iterating / 4loop the array to create buttons to display
-    displayCityData()
+    displayCityArray()
 }
 
 //invoke function to render persistent data independently
@@ -54,43 +55,50 @@ getCityArray()
 
 //take array and iterate thru to display dynamically, creating button element
 function displayCityArray() {
-    //create for loop
-   var cities = [];
-   for(let i = 0; i < 5; i++) {
-    cities.push(cities)
-   }
-    // for (let i = 0; i < cityArray.length; i++) {
-    //     const displayCityArray = cityarray [i];
-        
-    // }
-    //create button 
-}
-var buttonEl = document.getCreateElement("button")
-buttonEl.setAttribute("type","button")
-buttonEl.setAttribute("class","button-history")
-buttonEl.setAttribute("data-search", "array[0]")
-buttonEl.append("div")
 
-buttonEl.addEventListener("click", function () {
-    event.preventDefault()
-    console.log(weatherData)
-});
-    
+    //create for loop
+
+
+
+    for (let i = 0; i < cityDataArray.length; i++) {
+        //create button 
+        var btnEL = document.createElement("button")
+        btnEL.setAttribute("type", "button")
+        btnEL.setAttribute("class", "history-btn")
+        btnEL.setAttribute("data-search", cityDataArray[i])
+        btnEL.textContent = cityDataArray[i]
+        historyBtn.append(btnEL)
+    }
+
+}
+
+function cityHistoryHandler(e) {
+    // conditional if it doesnt match it wont return
+    if (!e.target.matches(".history-btn")) {
+        return
+    }
+    var btn = e.target
+    var city = btn.getattribute("data-search")
+
+
+    getApi(city)
+}
+historyBtn.addEventListener("click", cityHistoryHandler)
 
 // function getCityData(city) {
 //     console.log(city);
-    
+
 //     fetch(city)
 //      .then(function (response){
 //         return response.json();
 //      })
 //      .then(function (cityData){
 //         console.log(cityData[0]);
-       
+
 //         localStorage.setItem("cityData", JSON.stringify(cityData));
 
 //         cityDataArray = JSON.parse(localStorage.getItem("cityData"));
-         
+
 //         console.log(cityDateArray);
 //         if(cityDataArray  !== null) {
 //             console.log("Everything is good.");
@@ -100,10 +108,10 @@ buttonEl.addEventListener("click", function () {
 //       var cityData = document.getElementById("data-search")
 //     for (let i = 0; i < cityData.length; i++) {
 //         const localStorage.getitem = cityData[i];
-        
+
 //     }
 //     cityData()
-       
+
 // function getWeatherData(){
 //    var weatherData = cityInput.value
 //     var buttonEl = document.createElement("button");
@@ -138,22 +146,22 @@ formButton.addEventListener("click", function () {
 
 
 //function getFiveDayWeather(lat, lon) {
-    function getWeather(location) {
-        console.log(location)
-        var {lat,lon}= location
-        console.log(lat,lon)
-        var city = location.name
-        var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
+function getWeather(location) {
+    console.log(location)
+    var { lat, lon } = location
+    console.log(lat, lon)
+    var city = location.name
+    var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
     fetch(queryUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-           console.log(data)
-           //data.city.name,data.list[0],
-            currentDay(data.city.name,data.list[0]);
+            console.log(data)
+            //data.city.name,data.list[0],
+            currentDay(data.city.name, data.list[0]);
 
-           forecastDays(data.list)
+            forecastDays(data.list)
 
         });
 
@@ -165,147 +173,147 @@ function forecastDays(weather) {
     var forecastContainer = document.getElementById("forecastCard")
     for (let i = 0; i < weather.length; i++) {
         //First filters through all of the data and returns only data that falls between ond day after the current data and up to 5 days later.
-        if(weather[i].dt_text.slice(11, 13) == "12"){
-         const forecast = weather[i];
+        if (weather[i].dt_text.slice(11, 13) == "12") {
+            const forecast = weather[i];
 
-         //pulling data creating variables
-        var temp = weather.main.temp;
-        var humidity = weather.main.humidity;
-        var wind = weather.wind.speed;
-        var icon = `https:://openforecastmap.org/imp/w/${forecast.weather[0].icon.png}`;
-        var iconAlt = weather.weather[0].description
+            //pulling data creating variables
+            var temp = weather.main.temp;
+            var humidity = weather.main.humidity;
+            var wind = weather.wind.speed;
+            var icon = `https:://openforecastmap.org/imp/w/${forecast.weather[0].icon.png}`;
+            var iconAlt = weather.weather[0].description
 
- //creating card / cardbody
-         var card = document.createElement("div")
-         var cardbody = document.createElement("div")
-         card.setAttribute("class", "card")
- //add class and append to card
- cardbody.setAttribute("class", "card-body")
- card.append(cardbody)
+            //creating card / cardbody
+            var card = document.createElement("div")
+            var cardbody = document.createElement("div")
+            card.setAttribute("class", "card")
+            //add class and append to card
+            cardbody.setAttribute("class", "card-body")
+            card.append(cardbody)
 
- // create the elements for the cardbody
- var tempEl = document.createElement("p")
- var humidityEl = document.createElement("p")
- var windEl = document.createElement("p")
- var imgIconEl = document.createElement("img")
+            // create the elements for the cardbody
+            var tempEl = document.createElement("p")
+            var humidityEl = document.createElement("p")
+            var windEl = document.createElement("p")
+            var imgIconEl = document.createElement("img")
 
-//add the attributes like class, src, alt
-tempEl.setAttribute("class", "card-text")
-humidityEl.setAttribute("class", "card-text")
-windEl.setAttribute("class", "card-text")
+            //add the attributes like class, src, alt
+            tempEl.setAttribute("class", "card-text")
+            humidityEl.setAttribute("class", "card-text")
+            windEl.setAttribute("class", "card-text")
 
-//add contents to Element
+            //add contents to Element
 
-imgIconEl.setAttribute("src", icon)
-imgIconEl.setAttribute("alt", iconAlt)
-imgIconEl.setAttribute("class", "altIcon")
+            imgIconEl.setAttribute("src", icon)
+            imgIconEl.setAttribute("alt", iconAlt)
+            imgIconEl.setAttribute("class", "altIcon")
 
-tempEl.textContent = `Temp: ${temp} F`
-humidityEl.textContent = `Humidity: ${humidity} %`
-windEl.textContent = `Wind: ${wind} MPH`
+            tempEl.textContent = `Temp: ${temp} F`
+            humidityEl.textContent = `Humidity: ${humidity} %`
+            windEl.textContent = `Wind: ${wind} MPH`
 
-//append above to cardbody
-cardbody.append(imgIconEl, tempEl, windEl, humidityEl)
+            //append above to cardbody
+            cardbody.append(imgIconEl, tempEl, windEl, humidityEl)
 
-//append the card to html conainer for 5 day forecast
-forecastContainer.append(card)
+            //append the card to html conainer for 5 day forecast
+            forecastContainer.append(card)
         }
     }
 }
 
-function currentDay(city, weather){
+function currentDay(city, weather) {
     var date = dayjs().format('M/D/YYYY');
     console.log(city)
     console.log(weather)
 
     var todayContainer = document.getElementById("today")
-//pulling data creating variables
-var temp = weather.main.temp;
- var humidity = weather.main.humidity;
- var wind = weather.wind.speed;
- var icon = `https:://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
- var iconAlt = weather.weather[0].description
+    //pulling data creating variables
+    var temp = weather.main.temp;
+    var humidity = weather.main.humidity;
+    var wind = weather.wind.speed;
+    var icon = `https:://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+    var iconAlt = weather.weather[0].description
 
- //creating card / cardbody
- var card = document.createElement("div")
- var cardbody = document.createElement("div")
+    //creating card / cardbody
+    var card = document.createElement("div")
+    var cardbody = document.createElement("div")
 
- //add class
- card.setAttribute("class", "card")
- cardbody.setAttribute("class","card-body")
- //append cardbody to card (it goes inside the card "wrapper")
- card.append(cardbody)
+    //add class
+    card.setAttribute("class", "card")
+    cardbody.setAttribute("class", "card-body")
+    //append cardbody to card (it goes inside the card "wrapper")
+    card.append(cardbody)
 
- //create the elements for the cardbody
- 
- var title =  document.createElement("h3")
- var tempEl = document.createElement("p")
- var humidityEl= document.createElement("p")
- var windEl = document.createElement("p")
- var imgIconEl = document.createElement("img")
+    //create the elements for the cardbody
 
- //add the attributes like class, src, alt
+    var title = document.createElement("h3")
+    var tempEl = document.createElement("p")
+    var humidityEl = document.createElement("p")
+    var windEl = document.createElement("p")
+    var imgIconEl = document.createElement("img")
 
- title.setAttribute("class",  "h3 card-title")
- tempEl.setAttribute("class", "card-text")
- humidityEl.setAttribute("class", "card-text")
- windEl.setAttribute("class", "card-text")
+    //add the attributes like class, src, alt
 
- title.textContent = `${city} (${date})`;
+    title.setAttribute("class", "h3 card-title")
+    tempEl.setAttribute("class", "card-text")
+    humidityEl.setAttribute("class", "card-text")
+    windEl.setAttribute("class", "card-text")
 
- imgIconEl.setAttribute("src", icon)
-imgIconEl.setAttribute("class", "altIcon")
+    title.textContent = `${city} (${date})`;
 
-tempEl.textContent = `Temp: ${temp}`
-humidityEl.textContent = `Humidity: ${humidity}`
-windEl.textContent = `Wind: ${wind}`
+    imgIconEl.setAttribute("src", icon)
+    imgIconEl.setAttribute("class", "altIcon")
 
-//append above to cardbody: this acts as a "wrapper for the elements/inof to display"
+    tempEl.textContent = `Temp: ${temp}`
+    humidityEl.textContent = `Humidity: ${humidity}`
+    windEl.textContent = `Wind: ${wind}`
 
-cardbody.append(title, imgIconEl,tempEl, windEl, humidityEl)
-todayContainer.append(card)
- 
+    //append above to cardbody: this acts as a "wrapper for the elements/inof to display"
+
+    cardbody.append(title, imgIconEl, tempEl, windEl, humidityEl)
+    todayContainer.append(card)
+
     //temp, wind, humidity, icon
     //weather.main.temp, weather.main.humidity, weather.wind.speed
- //pulling data creating variables
- 
- //creating card
- var card = document.createElement("div")
- card.setAttribute("class", "card")
+    //pulling data creating variables
 
- //making card body
- var cardbody = document.createElement("div")
- cardbody.setAttribute("class", "card-body")
- card.append(cardbody)
+    //creating card
+    var card = document.createElement("div")
+    card.setAttribute("class", "card")
 
- // title 
- var title =  document.createElement("h3")
- title.setAttribute("class",  "h3 card-title")
- title.textContent = city;
+    //making card body
+    var cardbody = document.createElement("div")
+    cardbody.setAttribute("class", "card-body")
+    card.append(cardbody)
 
- //create elements to apply data to it, goes to card body
- var tempEl = document.createElement("p")
- var humidityEl = document.createElement("p")
- var windEl = document.createElement("p")
- var imgIconEl = document.createElement("img")
+    // title 
+    var title = document.createElement("h3")
+    title.setAttribute("class", "h3 card-title")
+    title.textContent = city;
 
-
-//setattributes of class, src and alt added to the element variabnles
- tempEl.setAttribute("class", "card-text")
- humidityEl.setAttribute("class", "card-text")
- windEl.setAttribute("class", "card-text")
- imgIconEl.setAttribute("src", icon)
- imgIconEl.setAttribute("alt", iconAlt)
- imgIconEl.setAttribute("class", "altIcon")
- //textcontent added to the element variables
-tempEl.textContent = `Temp: ${temp}`
-humidityEl.textContent = `Humidity: ${humidity}`
-windEl.textContent = `Wind: ${wind}`
+    //create elements to apply data to it, goes to card body
+    var tempEl = document.createElement("p")
+    var humidityEl = document.createElement("p")
+    var windEl = document.createElement("p")
+    var imgIconEl = document.createElement("img")
 
 
+    //setattributes of class, src and alt added to the element variabnles
+    tempEl.setAttribute("class", "card-text")
+    humidityEl.setAttribute("class", "card-text")
+    windEl.setAttribute("class", "card-text")
+    imgIconEl.setAttribute("src", icon)
+    imgIconEl.setAttribute("alt", iconAlt)
+    imgIconEl.setAttribute("class", "altIcon")
+    //textcontent added to the element variables
+    tempEl.textContent = `Temp: ${temp}`
+    humidityEl.textContent = `Humidity: ${humidity}`
+    windEl.textContent = `Wind: ${wind}`
 
-cardbody.append(title, imgIconEl, tempEl, windEl, humidityEl)
- todayContainer.append(card)
+
+
+    cardbody.append(title, imgIconEl, tempEl, windEl, humidityEl)
+    todayContainer.append(card)
 }
 // localStorage.setItem("forecast", forecastData);
 // localStorage.getItem("forecast");
